030244459b01c16f5b3033dfd0bd269ba01cfd87
Jarod Godlewski <jrg3321@rit.edu>
2017-04-03 22:50:32 -0400

    Created dist

--- ui/.gulp/tasks/assets.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp')
-
-module.exports = function (config) {
-  // move everything in the assets folder to distribution
-  return function () {
-    return gulp.src([ config.app.src + 'assets/**/*' ])
-      .pipe(gulp.dest(config.app.dist + 'assets'))
-  }
-}
--- ui/.gulp/tasks/clean.js
+++
@@ -1,9 +0,0 @@
-var clean = require('gulp-clean')
-var gulp = require('gulp')
-
-module.exports = function (config) {
-  // delete our distribution folder
-  return function () {
-    return gulp.src(config.app.dist).pipe(clean())
-  }
-}
