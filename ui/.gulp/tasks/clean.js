034931c752a8b4f2c2ae8ae63c45b95b9da74709
JarodGodlewski <jrg3321@rit.edu>
2017-04-05 20:12:28 -0400

    Update clean.js

--- ui/.gulp/tasks/clean.js
+++ ui/.gulp/tasks/clean.js
@@ -2,7 +2,6 @@ var clean = require('gulp-clean')
 var gulp = require('gulp')
 
 module.exports = function (config) {
-  // delete our distribution folder
   return function () {
     return gulp.src(config.app.dist).pipe(clean())
   }
