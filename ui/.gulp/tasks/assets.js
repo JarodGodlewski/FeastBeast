f0efa7ad2c30436877b56c15c7fa7655f075c866
JarodGodlewski <jrg3321@rit.edu>
2017-04-05 20:11:51 -0400

    Update assets.js

--- ui/.gulp/tasks/assets.js
+++ ui/.gulp/tasks/assets.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp')
 
 module.exports = function (config) {
-  // move everything in the assets folder to distribution
   return function () {
     return gulp.src([ config.app.src + 'assets/**/*' ])
       .pipe(gulp.dest(config.app.dist + 'assets'))
