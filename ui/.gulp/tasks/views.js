3e9a95a9595f72726a073e38509fc70dfc5d4250
JarodGodlewski <jrg3321@rit.edu>
2017-04-05 20:13:42 -0400

    Update views.js

--- ui/.gulp/tasks/views.js
+++ ui/.gulp/tasks/views.js
@@ -3,8 +3,6 @@ var minifyHtml = require('gulp-htmlmin')
 var tmplCache = require('gulp-angular-templatecache')
 
 module.exports = function (config) {
-  // move our html views to a template cache folder so each one
-  // doesn't need to be happen as an ajax request
   return function () {
     return gulp.src(config.files.views)
       .pipe(minifyHtml({ collapseWhitespace: true }))
