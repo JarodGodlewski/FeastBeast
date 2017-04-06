84f87984ee1e3ab11bb96375050959ac8cbe9fda
JarodGodlewski <jrg3321@rit.edu>
2017-04-05 20:13:07 -0400

    Update scripts.js

--- ui/.gulp/tasks/scripts.js
+++ ui/.gulp/tasks/scripts.js
@@ -2,7 +2,6 @@ var concat = require('gulp-concat')
 var gulp = require('gulp')
 
 module.exports = function (config) {
-  // move script files over
   return function () {
     var stream = gulp.src(config.files.scripts)
     return stream.pipe(gulp.dest(config.app.dist))
